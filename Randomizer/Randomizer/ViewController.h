cdfbbbd8caf6653f6b37e1d267d5a79a80e99db7
Hyrum Mertlich <hmertlich@me.com>
2015-08-12 09:31:18 -0600

    Initial
    This is the start of my project.

---
+++ Randomizer/Randomizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Randomizer
+//
+//  Created by Hyrum Mertlich on 8/12/15.
+//  Copyright (c) 2015 M Auto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
